4dd84253a9accd51e6c8dc286c30cfe28156a5c2
Mariano Vargas <marianoluisvargas@gmail.com>
2024-01-25 16:42:57 -0300

    feat(MinChange): MinChange moved to it's own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavBar } from "./components/NavBar/NavBar";
-import { TextField, Button, Typography } from "@mui/material";
-import minChange from "./helpers/minChange";
 import { FindPair } from "./components/FindPair/FindPair";
+import { MinChange } from "./components/MinChange/MinChange";
 
 function App() {
-  const [coinsInput, setCoinsInput] = useState("");
-  const [minChangeResult, setMinChangeResult] = useState("");
-
-  const [numbersInput, setNumbersInput] = useState("");
-  const [targetInput, setTargetInput] = useState("");
-  const [findPairResult, setFindPairResult] = useState("");
-
-  const handleMinChange = () => {
-    try {
-      const coins = coinsInput.split(",").map((value) => Number(value.trim()));
-      const result = minChange(coins);
-      setMinChangeResult(`Minimum change: ${result}`);
-    } catch (error) {
-      setMinChangeResult("Invalid input");
-    }
-  };
-
   return (
     <div>
       <NavBar />
-      <div>
-        <Typography variant='h6' sx={{ textAlign: "center" }}>
-          Min Change
-        </Typography>
-        <TextField label='Coins (Array)' variant='outlined' value={coinsInput} onChange={(e) => setCoinsInput(e.target.value)} />
-        <Button variant='contained' color='primary' onClick={handleMinChange}>
-          Run minChange
-        </Button>
-        <div>{minChangeResult}</div>
-      </div>
       <FindPair />
+      <MinChange />
     </div>
   );
 }
--- src/components/MinChange/MinChange.jsx
+++ src/components/MinChange/MinChange.jsx
@@ -1,3 +1,30 @@
+import { useState } from "react";
+import { TextField, Button, Typography } from "@mui/material";
+import minChange from "../../helpers/minChange";
+
 export const MinChange = () => {
-  return <div>MinChange</div>;
+  const [coinsInput, setCoinsInput] = useState("");
+  const [minChangeResult, setMinChangeResult] = useState("");
+
+  const handleMinChange = () => {
+    try {
+      const coins = coinsInput.split(",").map((value) => Number(value.trim()));
+      const result = minChange(coins);
+      setMinChangeResult(`Minimum change: ${result}`);
+    } catch (error) {
+      setMinChangeResult("Invalid input");
+    }
+  };
+  return (
+    <div>
+      <Typography variant='h6' sx={{ textAlign: "center" }}>
+        Min Change
+      </Typography>
+      <TextField label='Coins (Array)' variant='outlined' value={coinsInput} onChange={(e) => setCoinsInput(e.target.value)} />
+      <Button variant='contained' color='primary' onClick={handleMinChange}>
+        Run minChange
+      </Button>
+      <div>{minChangeResult}</div>
+    </div>
+  );
 };
